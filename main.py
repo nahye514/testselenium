9506a998dc5b0da028394f27c7036a51bb22ed91
nahye514 <nahye514@gmail.com>
2022-05-28 14:10:34 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+class BaseLayer:
+    def __init__(self, name=""):
+        self.name = name
+
+    def __repr__(self):
+        return f"{self.name}Layer"
+
+class ActivationLayer(BaseLayer):
+    def __init__(self, size):
+        super().__init__("Activation")
+        self.size = size
+
+class FCLayer(BaseLayer):
+    def __init__(self, size):
+        super().__init__("FullyConnected")
+        self.size = size
+
+print(FCLayer(42))
+
+print(BaseLayer(4).size)
